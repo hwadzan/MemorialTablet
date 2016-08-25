87737115f358090f8cff5b82b5383638d1df875e
Yuan-Chen Cheng <ycheng.tw@gmail.com>
2016-08-25 09:44:50 +0800

    add background.GIF, and print preview code.

--- MemorialTabletBasic/Form1.Designer.vb
+++ MemorialTabletBasic/Form1.Designer.vb
@@ -22,9 +22,53 @@ Partial Class Form1
     '請勿使用程式碼編輯器進行修改。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnChoosePrinter = New System.Windows.Forms.Button()
+        Me.btnPrintPreview = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(363, 27)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(141, 41)
+        Me.btnPrint.TabIndex = 0
+        Me.btnPrint.Text = "直接印列"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnChoosePrinter
+        '
+        Me.btnChoosePrinter.Location = New System.Drawing.Point(24, 27)
+        Me.btnChoosePrinter.Name = "btnChoosePrinter"
+        Me.btnChoosePrinter.Size = New System.Drawing.Size(141, 41)
+        Me.btnChoosePrinter.TabIndex = 1
+        Me.btnChoosePrinter.Text = "選擇印表機"
+        Me.btnChoosePrinter.UseVisualStyleBackColor = True
+        '
+        'btnPrintPreview
+        '
+        Me.btnPrintPreview.Location = New System.Drawing.Point(195, 27)
+        Me.btnPrintPreview.Name = "btnPrintPreview"
+        Me.btnPrintPreview.Size = New System.Drawing.Size(141, 41)
+        Me.btnPrintPreview.TabIndex = 2
+        Me.btnPrintPreview.Text = "印列預覽"
+        Me.btnPrintPreview.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(540, 97)
+        Me.Controls.Add(Me.btnPrintPreview)
+        Me.Controls.Add(Me.btnChoosePrinter)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnChoosePrinter As Button
+    Friend WithEvents btnPrintPreview As Button
 End Class
--- MemorialTabletBasic/Form1.vb
+++ MemorialTabletBasic/Form1.vb
@@ -1,3 +1,57 @@
-﻿Public Class Form1
+﻿Imports System.Drawing.Printing
 
+Public Class Form1
+
+    Private printPreviewDiag As New PrintPreviewDialog()
+    Private printDiag As New PrintDialog
+
+    Private WithEvents printDoc As New PrintDocument()
+
+    ' default page settings from default printer
+    Private currentPageSettings As New PageSettings
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnChoosePrinter_Click(sender As Object, e As EventArgs) Handles btnChoosePrinter.Click
+        Dim ret As DialogResult
+
+        ret = printDiag.ShowDialog()
+        If ret = DialogResult.OK Then
+            currentPageSettings = New PageSettings
+        End If
+    End Sub
+
+    Private Sub btnPrintPreview_Click(sender As Object, e As EventArgs) Handles btnPrintPreview.Click
+        printPreviewDiag.Document = printDoc
+        printPreviewDiag.ShowDialog()
+    End Sub
+
+    Sub printDocument1_PrintPage(ByVal sender As Object,
+        ByVal e As PrintPageEventArgs) Handles printDoc.PrintPage
+
+        Dim bx, by As Single
+        Dim x, y As Single
+
+        bx = e.MarginBounds.X
+        by = e.MarginBounds.Y
+
+        Dim scale As Single
+        Dim ox, oy As Single
+
+        ox = My.Resources.background.Size.Width
+        oy = My.Resources.background.Size.Height
+        scale = e.MarginBounds.Width / ox
+        x = ox * scale
+        y = oy * scale
+
+        If 1 Then
+            e.Graphics.DrawImage(My.Resources.background,
+                New System.Drawing.RectangleF(bx, by, x, y),
+                New System.Drawing.RectangleF(0, 0, ox, oy), System.Drawing.GraphicsUnit.Pixel)
+            ' e.Graphics.DrawImage(My.Resources.background, New PointF(0, 0))
+        End If
+        e.HasMorePages = False
+    End Sub
 End Class
