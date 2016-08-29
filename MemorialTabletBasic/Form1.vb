b440f8a54f49f60413dd5a7ee3daed9a5e19d135
Yuan-Chen Cheng <ycheng.tw@gmail.com>
2016-08-29 21:55:08 +0800

    Rotate background. Print Chinese in landscape. (now works !)

--- MemorialTabletBasic/Form1.vb
+++ MemorialTabletBasic/Form1.vb
@@ -8,10 +8,14 @@ Public Class Form1
     Private WithEvents printDoc As New PrintDocument()
 
     ' default page settings from default printer
-    Private currentPageSettings As New PageSettings
+    Private currentPrinterSettings As New PrinterSettings
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        If False Then
+            printDoc.DefaultPageSettings.Landscape = True
+            printPreviewDiag.Document = printDoc
+            printPreviewDiag.ShowDialog()
+        End If
     End Sub
 
     Private Sub btnChoosePrinter_Click(sender As Object, e As EventArgs) Handles btnChoosePrinter.Click
@@ -19,13 +23,54 @@ Public Class Form1
 
         ret = printDiag.ShowDialog()
         If ret = DialogResult.OK Then
-            currentPageSettings = New PageSettings
+            currentPrinterSettings = printDiag.PrinterSettings
         End If
     End Sub
 
     Private Sub btnPrintPreview_Click(sender As Object, e As EventArgs) Handles btnPrintPreview.Click
+        printDoc.PrinterSettings = currentPrinterSettings
         printPreviewDiag.Document = printDoc
         printPreviewDiag.ShowDialog()
+    End Sub
+
+    ' 往生蓮位
+    Private Sub drawMemo1(g As Graphics, tX As Single, tY As Single, tWidth As Single, tHeight As Single,
+                          name1 As String, name2 As String) ' name1 先人, name2 陽上
+        Dim sWidth, sHeight As Single
+        Dim scale
+        Dim scaleD = 0.188147
+        Dim scaleAdj As Single
+        Dim fontC As Font
+
+        sWidth = My.Resources.background.Size.Width
+        sHeight = My.Resources.background.Size.Height
+
+        scale = tWidth / sWidth
+        scaleAdj = scale / scaleD
+        g.DrawImage(My.Resources.background,
+                    New System.Drawing.RectangleF(tX, tY, tWidth, tHeight),
+                    New System.Drawing.RectangleF(0, 0, sWidth, sHeight),
+                    System.Drawing.GraphicsUnit.Pixel)
+
+        Dim c1 As String = "佛力超薦"
+        Dim c2 As String = "往生蓮位"
+
+        Dim l1 As String = "陽上"
+
+        fontC = New Font("新細明體", Int(20 * scaleAdj))
+
+        ' of stringToPrint that will fit within the bounds of the page.
+        ' g.MeasureString(c1, Me.Font, e.MarginBounds.Size,
+        ' StringFormat.GenericTypographic, charactersOnPage, linesPerPage)
+
+        Dim drawFormat As New System.Drawing.StringFormat
+        drawFormat.FormatFlags = StringFormatFlags.DirectionVertical
+
+        ' Draws the string within the bounds of the page.
+        ' g.DrawString(c1, Me.Font, Brushes.Black, New RectangleF(tX, tY, tWidth, tHeight), StringFormat.GenericTypographic)
+        ' g.DrawString(c1, font1, Brushes.Black, New RectangleF(tX, tY, tWidth, tHeight), StringFormat.GenericTypographic)
+        g.DrawString(c1, fontC, Brushes.Black, New RectangleF(tX + 110 * scaleAdj, tY + 165 * scaleAdj, 30 * scaleAdj, tHeight), drawFormat)
+        g.DrawString(c2, fontC, Brushes.Black, New RectangleF(tX + 110 * scaleAdj, tY + 525 * scaleAdj, 30 * scaleAdj, tHeight), drawFormat)
     End Sub
 
     Sub printDoc_PrintPage(ByVal sender As Object,
@@ -65,17 +110,14 @@ Public Class Form1
         Else
             scale = scaleY
         End If
-        scale = scale / 2
+        ' scale = scale / 2
 
         tWidth = sWidth * scale
         tHeight = sHeight * scale
 
         For ix = 1 To Int(tPageWidth / tWidth)
             For iy = 1 To Int(tPageHeight / tHeight)
-                e.Graphics.DrawImage(My.Resources.background,
-                    New System.Drawing.RectangleF(tPageX + (ix - 1) * tWidth, tPageY + (iy - 1) * tHeight, tWidth, tHeight),
-                    New System.Drawing.RectangleF(0, 0, sWidth, sHeight),
-                    System.Drawing.GraphicsUnit.Pixel)
+                drawMemo1(e.Graphics, tPageX + (ix - 1) * tWidth, tPageY + (iy - 1) * tHeight, tWidth, tHeight, "n1", "n2")
             Next
         Next
         e.HasMorePages = False
